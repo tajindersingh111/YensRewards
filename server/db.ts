240ce949f3d4c596e4f09fbc48d6fe221a8b05ae
tajindersigh111 <tajindsingh012@gmail.com>
2026-05-22 16:03:03 +0530

    fix postgres ssl

--- server/db.ts
+++ server/db.ts
@@ -8,6 +8,9 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const db = drizzle(pool, { schema });
